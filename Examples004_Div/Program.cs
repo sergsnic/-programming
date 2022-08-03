0f810825745c6dfc626d7badf1921334716a47e7
Sergey <sergsnic@mail.ru>
2022-08-03 15:59:19 +0300

    add Examples004_Div/Examples004_Div

---
+++ Examples004_Div/Program.cs
@@ -0,0 +1,6 @@
+﻿// пример 3 лекция 1
+Console.Write("Введите первое число: ");
+int numeric1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int numeric2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Деление двух чисел будет равно: " + (Convert.ToDouble(numeric1) / Convert.ToDouble(numeric2)));
