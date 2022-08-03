5d024da14b7d1eb72424310c04615b77a173ea8c
Sergey <sergsnic@mail.ru>
2022-08-03 16:27:31 +0300

    add Examples005_If_Else/Examples005_If_Else

---
+++ Examples005_If_Else/Program.cs
@@ -0,0 +1,11 @@
+﻿// 5 программа на С# if else урок 1
+Console.WriteLine("Введите имя пользователя");
+string username = Console.ReadLine();
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("Салют Маша!!!");
+}
+else
+{
+    Console.WriteLine("Привет " + username);
+}
